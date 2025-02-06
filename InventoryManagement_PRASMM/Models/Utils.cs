896ef72945a94aef4ce6134e0ee47fa5fee99601
Alvin Malapitan <alvinmalapitan08@gmail.com>
2025-02-06 23:09:02 +0800

    Detail parameter encryption left

--- InventoryManagement_PRASMM/Models/Utils.cs
+++ InventoryManagement_PRASMM/Models/Utils.cs
@@ -11,5 +11,23 @@ namespace InventoryManagement_PRASMM.Models
             int returnValue = dal.GetNewTransactionID(id);
             return returnValue;
         }
+        public void transactionRollback() {
+            var dal =new UtilsDAL();
+            dal.End(true);
+        }
+
+        public string Decrypt(string cypherString, bool useHasing)
+        {
+            var dal = new UtilsDAL();
+            string decryptedString = dal.Decrypt(cypherString, useHasing);
+            return decryptedString;
+        }
+
+        public string Encrypt(string cypherString, bool useHasing) 
+        { 
+            var dal = new UtilsDAL();
+            string encryptedString = dal.Encrypt(cypherString,useHasing);
+            return encryptedString;
+        }
     }
 }
